d854abb683ad761e15fee2e9f54aaaa0da483998
Hemant <hemant@hemant-VirtualBox.(none)>
2013-03-27 20:26:36 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railtest::Application.config.secret_token = '583a7a75bddcf31043a376ca33caaa12f1a25c69c3a3a7ceca5e3597dbf659904b967a64fec0f1fec571ed0f964b579a3d5de84fd913f281f36026ac65a4c342'
